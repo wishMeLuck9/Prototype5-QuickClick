81db3fa629ea5d13726ffa8342afd41149a2291a
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-06-18 05:33:59 +0100

    Import official Prototype 5 starter assets

---
+++ Assets/Course Library/_Source_Files/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OfficialStarterAssets
+{
+public class DestroyAfterDelay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
